8477d5ea124d1b8beea6645b9674a79d36284363
os.kwon <93negaro@gmail.com>
2020-04-07 01:53:00 +0900

    redux-saga를 이용하여 회원가입 및 로그인 기능 구현 준비

--- blog-frontend/src/components/common/Button.js
+++ blog-frontend/src/components/common/Button.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
 import palette from '../../lib/styles/palette';
 
-const StyledButton = styled.button`
+const buttonStyle = css`
   border: none;
   border-radius: 4px;
   font-size: 1rem;
@@ -16,8 +17,40 @@ const StyledButton = styled.button`
   &:hover {
     background: ${palette.gray[6]};
   }
-  `;
 
-const Button = props => <StyledButton {...props} />;
+  ${props =>
+    props.fullWidth &&
+    css`
+      padding-top: 0.75rem;
+      padding-bottom: 0.75rem;
+      width: 100%;
+      font-size: 1.125rem;
+    `}
+
+  ${props =>
+    props.cyan &&
+    css`
+      background: ${palette.cyan[5]};
+      &:hover {
+        background: ${palette.cyan[4]};
+      }
+    `}
+`;
+
+const StyledButton = styled.button`
+  ${buttonStyle}
+`;
+
+const StyledLink = styled(Link)`
+  ${buttonStyle}
+`;
+
+const Button = props => {
+  return props.to ? (
+    <StyledLink {...props} cyan={props.cyan ? 1 : 0} />
+  ) : (
+    <StyledButton {...props} />
+  );
+};
 
 export default Button;
